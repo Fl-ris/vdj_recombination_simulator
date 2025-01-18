0c9b516a08a471ad3992ea772a88da58db6bbf8c
Fl-ris <florismenninga@gmail.com>
2025-01-18 23:03:47 +0100

    Added logic to check if a sequence is in frame relative to the start codon.

--- vdj_recombinator.py
+++ vdj_recombinator.py
@@ -42,17 +42,17 @@ def rss_finder(seq_file):
 
     # 12bp spacer van nonameer naar heptameer <
     rss_regex_12bp_9_7 = "GGTTTTTGT.{12}CACTGTG"
-    
     # 12bp spacer van heptameer naar nanomeer >
   # rss_regex_12bp_7_9 = "CACTGTG.{12}GGTTTTTGT"  # Motief volgens powerpoint slide (Geen matches)
-    # Volgens dit artikel is er ook een motif: https://pmc.ncbi.nlm.nih.gov/articles/PMC308075/
+
+    # Volgens dit artikel is er ook een ander motief: https://pmc.ncbi.nlm.nih.gov/articles/PMC308075/
     rss_regex_12bp_7_9 = "CACAGTG.{12}ACAAAAACC"
 
 
 
     # 23bp spacer nonameer naar heptameer <
-    rss_regex_23bp_9_7 = "ACAAAAACC.{23}CACAGTG"
-                          
+  #  rss_regex_23bp_9_7 = "ACAAAAACC.{23}CACAGTG"   # Motief volgens powerpoint slide (Geen matches)
+    rss_regex_23bp_9_7 = "GGTTTTTGT.{23}CACTGTG" # Complementaire sequentie hiervan had wel matches. 
     # 23bp spacer heptameer naar nonameer >
     rss_regex_23bp_7_9 = "CACAGTG.{23}ACAAAAACC"
 
@@ -61,11 +61,10 @@ def rss_finder(seq_file):
     rss_regex_12bp_7_9_list = re.findall(rss_regex_12bp_7_9, seq_file)
 
     # Placeholder:
-    rss_regex_23bp_9_7_list = re.findall(rss_regex_23bp_7_9, seq_file)
-
+    rss_regex_23bp_9_7_list = re.findall(rss_regex_23bp_9_7, seq_file)
     rss_regex_23bp_7_9_list = re.findall(rss_regex_23bp_7_9, seq_file)
 
-    print(f"Rss 23bp: {rss_regex_12bp_9_7_list} RSS 12bp: ")
+    #print(f"Rss 23bp: {rss_regex_12bp_9_7_list} RSS 12bp: ")
 
 
     #char_after_seq(seq_file, rss_regex_12bp_list[1], 20)
@@ -73,7 +72,9 @@ def rss_finder(seq_file):
     return rss_regex_12bp_9_7_list, rss_regex_12bp_7_9_list, rss_regex_23bp_9_7_list, rss_regex_23bp_7_9_list
 
 def v_finder(seq_file, rss_regex_23bp_9_7_list, rss_regex_23bp_7_9_list):
-
+    """
+    Zoek het ATG tot eerste 23bp RSS segment en zoek v_segmenten: tussen twee 23bp RSS'en.
+    """
     # ATG tot de 23bp RSS (V segment)
     start_to_23_rss_motif = "(ATG.*)(CACAGTG.{23}ACAAAAACC)"
 
@@ -82,45 +83,65 @@ def v_finder(seq_file, rss_regex_23bp_9_7_list, rss_regex_23bp_7_9_list):
     rand_rss_regex_23bp_9_7 = random.choice(rss_regex_23bp_9_7_list)
     rand_rss_regex_23bp_7_9 = random.choice(rss_regex_23bp_7_9_list)
     
+    v_motif = "(CACAGTG.{23}ACAAAAACC).*(CACAGTG.{23}ACAAAAACC)"
+    v_segment = re.search(v_motif, seq_file)
+
     # Gebruik re.search i.p.v. findall omdat er anders een korte sequentie bij zit. (Voor onbekende reden)
     start_to_23_rss = re.search(start_to_23_rss_motif, seq_file)
 
- #   print(rand_rss_regex_12bp_7_9)
+    #print(start_to_23_rss[1])
+    return start_to_23_rss, v_segment
 
 
     
-
 def d_finder(seq_file,  rss_regex_12bp_9_7_list, rss_regex_12bp_7_9_list):
 
-   # Het d segment hoort tussen 
-   # d_motif = rand_rss_regex_12bp_9_7 tot rand_rss_regex_12bp_7_9
+   # Het d segment hoort tussen rand_rss_regex_12bp_9_7 tot rand_rss_regex_12bp_7_9
 
+    found_valid_motif = False
+    tries = 0 # Zodat het niet oneindig door blijft gaan.
+    while found_valid_motif == False and tries < 20:
 
-    rand_rss_regex_12bp_9_7 = random.choice(rss_regex_12bp_9_7_list)
-    rand_rss_regex_12bp_7_9 = random.choice(rss_regex_12bp_7_9_list)
+        # Try except is nodig omdat hij niet altijd wat kan matchen en in het geval dat het niet kan geeft het een foutmelding.
+        try:
+            rand_rss_regex_12bp_9_7 = random.choice(rss_regex_12bp_9_7_list)
+            rand_rss_regex_12bp_7_9 = random.choice(rss_regex_12bp_7_9_list)
 
-    d_motif = f"{rand_rss_regex_12bp_9_7}(.*){rand_rss_regex_12bp_7_9}"
-    start_to_23_rss = re.search(d_motif, seq_file)
+            d_motif = f"{rand_rss_regex_12bp_9_7}(.*){rand_rss_regex_12bp_7_9}"
+            d_segment = re.search(d_motif, seq_file)
 
-
-    # Try except is nodig omdat hij niet altijd wat kan matchen en in het geval dat het niet kan geeft het een foutmelding....
-    try:
-        print(f"D-segment: {start_to_23_rss[1]}")
-    except:
-        print("Random gekozen motief kon niet matchen, volgende keer beter.")
-
-
+            print(f"D-segment: {d_segment[1]}")
+            found_valid_motif = True
+            return d_segment
+        
+        except:
+            print("Random gekozen motief kon niet matchen, misschien de volgende iteratie wel...")
+            tries += 1
+    print("Random gekozen motief kon niet matchen na 20 pogingen.")
+            
+        
 
 def j_finder():
     pass
 
-def in_frame_check():
-    pass
+
+
+
+def in_frame_check(sequence_position):
+    """
+    Stel vast of het verkergen segment wel in frame is ten opzichte van het start codon.
+    De modulo wordt gebruikt zodat gekeken kan worden of de sequentie een meervoud van 3 (een codon) er van weg is. 
+    """
+
+    return (sequence_position % 3 == 0)
 
 def stop_condon_check():
     pass
 
 def print_resultaat():
+
+
+    # To-do: constant chain toevoegen aan eind. 
     pass
 
 
@@ -129,7 +150,8 @@ if __name__ == "__main__":
 
     rss_regex_12bp_9_7_list, rss_regex_12bp_7_9_list, rss_regex_23bp_9_7_list, rss_regex_23bp_7_9_list = rss_finder(seq_file)
 
-    v_finder(seq_file, rss_regex_23bp_9_7_list, rss_regex_23bp_7_9_list)
+    start_to_23_rss, v_segment = v_finder(seq_file, rss_regex_23bp_9_7_list, rss_regex_23bp_7_9_list)
 
-    d_finder(seq_file, rss_regex_12bp_9_7_list, rss_regex_12bp_7_9_list)
-  #  read_seq()
+    d_segment = d_finder(seq_file, rss_regex_12bp_9_7_list, rss_regex_12bp_7_9_list)
+
+    print(in_frame_check(2))
